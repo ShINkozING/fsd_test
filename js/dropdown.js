e03cdb87481ae0280879115bbc217a50bb1b54ae
Aleksandr Troshin <sshinkyeah@gmail.com>
2020-09-14 22:28:26 +0500

    edit form elements

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,11 +1,9 @@
 
 let select = function () {
     let selectHeader = document.querySelectorAll('.select__header');
-
     selectHeader.forEach(item => {
         item.addEventListener('click', selectToggle)
     });
-
     function selectToggle() {
         this.parentElement.classList.toggle('is-active');
     }
