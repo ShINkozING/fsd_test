4fe6cba5b5b61eacc350b6db15123a927e4ac07f
Aleksandr Troshin <sshinkyeah@gmail.com>
2020-09-15 17:48:00 +0500

    update dropdown js and css element

--- js/dropdown.js
+++ js/dropdown.js
@@ -13,23 +13,44 @@ select();
 
 //счетчик
 //найти кнопку +
-let buttonPlus = document.querySelector('[data-action="plus"]');
-let buttonMinus = document.querySelector('[data-action="minus"]')
+let buttonPlus = document.querySelectorAll('[data-action="plus"]');
+let buttonMinus = document.querySelectorAll('[data-action="minus"]')
 
-buttonPlus.addEventListener('click',()=>{
-    let inp = document.querySelector('.first');
-    let num = parseInt(inp.value, 10);
-    if(num <10){
-        num += 1;
-        inp.value = num;
-    }
+buttonPlus.forEach(function(item){
+
+    item.addEventListener('click', function(){
+        let counterWrapper = this.closest('.counters-wrapper');
+        let counter = counterWrapper.querySelector('[data-counter]');
+        if(counter.value < 10) {
+            counter.value = ++counter.value;
+        }
+    });
 });
 
-buttonMinus.addEventListener('click',()=>{
-    let inp = document.querySelector('.first');
-    let num = parseInt(inp.value, 10);
-    if (num > 0){
-        num -= 1;
-        inp.value = num;
-    } 
-});
+buttonMinus.forEach(function(item){
+    item.addEventListener('click', function(){
+        let counterWrapper = this.closest('.counters-wrapper');
+        let counter = counterWrapper.querySelector('[data-counter]');
+        if(counter.value > 0) {
+            counter.value = --counter.value;
+        }
+    });
+})
+
+// buttonPlus.addEventListener('click',()=>{
+//     let inp = document.querySelector('.first');
+//     let num = parseInt(inp.value, 10);
+//     if(num <10){
+//         num += 1;
+//         inp.value = num;
+//     }
+// });
+
+// buttonMinus.addEventListener('click',()=>{
+//     let inp = document.querySelector('.first');
+//     let num = parseInt(inp.value, 10);
+//     if (num > 0){
+//         num -= 1;
+//         inp.value = num;
+//     } 
+// });
